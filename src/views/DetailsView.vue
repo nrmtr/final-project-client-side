898f903dd4b7c820710755e2e1b1d7bb76632cd8
blue290446 <noo58237@gmail.com>
2025-01-23 22:35:51 +0700

    try update css

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,34 +1,43 @@
 <template>
-    <div class="details-view">
-      <h1>รายละเอียดมือถือ: {{ phone }}</h1>
-      <!-- ดึงข้อมูลมือถือเพิ่มเติมจาก API -->
-    </div>
-  </template>
+  <div class="details-view">
+    <h1>รายละเอียดมือถือ: {{ selectedphones }}</h1>
+    <!-- ดึงข้อมูลมือถือเพิ่มเติมจาก API -->
+  </div>
+</template>
   
   <script lang="ts">
-  import { defineComponent } from 'vue'
-  import axios from 'axios'
-  
-  export default defineComponent({
-    data() {
+import { defineComponent } from 'vue'
+import axios from 'axios'
+
+export default defineComponent({
+  data() {
     return {
-      phone: [] as string[],
+      selectedphones: [] as string[],
       apicompare: null as ApiResponse | null,
     }
   },
-    methods: {
+  mounted() {
+    const query = this.$route.query.mobiles
+    if (query) {
+    this.selectedphones = JSON.parse(this.$route.query.mobiles)
+    this.fetchdetails()
+  } else {
+      alert('ไม่มีข้อมูลสำหรับเปรียบเทียบ')
+    }
+  },
+  methods: {
     async fetchdetails() {
-      try {
-        const payload = {
-          phones: this.phone,
-        }
-        console.log('Payload being sent:', payload)
-
-        const response = await axios.post('http://13.251.160.30/api/phone/phone_details', payload, {
+      // try {
+        const selectedPhones = this.selectedphones.join(','); 
+        const url = `http://13.251.160.30/api/phone/phone_details/${selectedPhones}`;
+        console.log('Response url:', url)
+        const response = await axios.get(url, {
           headers: {
             'Content-Type': 'application/json',
           },
         })
+
+        console.log('Response received:', response.data)
 
         // Transform the API response to an array of phones
         const phones = Object.values(response.data)
@@ -49,17 +58,17 @@
         }
 
         console.log('API compare:', this.apicompare)
-      } catch (error) {
-        console.error('Error fetching brand details:', error)
-        this.apicompare = null
-        alert('ไม่สามารถดึงข้อมูลได้. กรุณาลองใหม่อีกครั้ง')
-      }
+      // } catch (error) {
+      //   console.error('Error fetching brand details:', error)
+      //   this.apicompare = null
+      //   alert('ไม่สามารถดึงข้อมูลได้. กรุณาลองใหม่อีกครั้ง')
+      // }
     },
   },
-  })
-  </script>
+})
+</script>
   
   <style scoped>
-  /* Add your component styles here */
-  </style>
+/* Add your component styles here */
+</style>
   
